672a0df71dba237884e3e15a10ff7a12bbf47bce
Marco-Zechner <marcoflorian00956@gmail.com>
2025-09-27 22:05:42 +0200

    setup

---
+++ src/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeDraw.Net;
+
+public class Class1
+{
+    public void TestMethod()
+    {
+        Console.WriteLine("Hello from CodeDraw.Net");
+    }
+}
---
+++ tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using CodeDraw.Net;
+
+namespace CodeDraw.NetTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // This is a test program
+        var test = new Class1();
+        test.TestMethod();
+        Console.WriteLine("Test method executed successfully.");
+    }
+}

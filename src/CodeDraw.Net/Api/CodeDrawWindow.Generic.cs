fcc27ddfa09625d96d5e716aa31662cff2e89768
Marco-Zechner <marcoflorian00956@gmail.com>
2025-11-11 03:35:18 +0100

    window starts again. (still errors in test 2, and missing functionality in test 1-3)

--- src/CodeDraw.Net/Api/CodeDrawWindow.Generic.cs
+++ src/CodeDraw.Net/Api/CodeDrawWindow.Generic.cs
@@ -3,11 +3,11 @@ using MarcoZechner.CodeDrawDotNet.Interfaces;
 namespace MarcoZechner.CodeDrawDotNet.Api;
 
 public unsafe class CodeDrawWindow<TRenderer> : CodeDrawWindowBase
-    where TRenderer : IAttachableRenderer,  new()
+    where TRenderer : IAttachableRenderer, IHostBootstrap<TRenderer>, new()
 {
     public CodeDrawWindow(string title) : base(title)
     {
-        // TRenderer.EnsureHost(); // IHostBootstrap<TRenderer>, //todo do we need this?
+        TRenderer.EnsureHost(); //todo do we need this? yes for now
     }
 
     protected override IAttachableRenderer CreateRenderer() => new TRenderer();

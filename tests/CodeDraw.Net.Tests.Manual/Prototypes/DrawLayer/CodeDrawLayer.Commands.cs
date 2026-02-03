f757f65b339d5903f6a3f23c079acce5e8323bde
Marco-Zechner <marcoflorian00956@gmail.com>
2026-02-03 03:50:27 +0100

    cleanup

--- tests/CodeDraw.Net.Tests.Manual/Prototypes/DrawLayer/CodeDrawLayer.Commands.cs
+++ tests/CodeDraw.Net.Tests.Manual/Prototypes/DrawLayer/CodeDrawLayer.Commands.cs
@@ -4,23 +4,23 @@ namespace MarcoZechner.CodeDrawDotNet.Tests.Manual.Prototypes.DrawLayer;
 
 public sealed unsafe partial class CodeDrawLayer
 {
-    private interface ICmd { void Exec(GL gl, DrawLayer.CodeDrawLayer self); }
+    private interface ICmd { void Exec(GL gl, CodeDrawLayer self); }
 
     private sealed class CmdSetBlendMode : ICmd
     {
         public BlendMode Mode;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self) { self._blendMode = Mode; self.ApplyBlendMode(); }
+        public void Exec(GL gl, CodeDrawLayer self) { self._blendMode = Mode; self.ApplyBlendMode(); }
     }
 
     private sealed class CmdSetBlitShader : ICmd
     {
         public CodeDrawShader? Shader;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self) => self._customBlitShader = Shader is { IsDisposed: false } ? Shader : null;
+        public void Exec(GL gl, CodeDrawLayer self) => self._customBlitShader = Shader is { IsDisposed: false } ? Shader : null;
     }
 
     private sealed class CmdClear(float r, float g, float b, float a) : ICmd
     {
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self)
+        public void Exec(GL gl, CodeDrawLayer self)
         {
             self._clearColor = (r, g, b, a);
             gl.ClearColor(self._clearColor.r, self._clearColor.g, self._clearColor.b, self._clearColor.a);
@@ -32,13 +32,13 @@ public sealed unsafe partial class CodeDrawLayer
     {
         public float X, Y, W, H;
         public float R, G, B, A;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self) => self.ExecRect(gl, X, Y, W, H, R, G, B, A);
+        public void Exec(GL gl, CodeDrawLayer self) => self.ExecRect(gl, X, Y, W, H, R, G, B, A);
     }
 
     private sealed class CmdLayer : ICmd
     {
-        public DrawLayer.CodeDrawLayer? Src;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self)
+        public CodeDrawLayer? Src;
+        public void Exec(GL gl, CodeDrawLayer self)
         {
             var s = Src;
             if (s is null || s._disposed) return;
@@ -49,12 +49,12 @@ public sealed unsafe partial class CodeDrawLayer
     private sealed class CmdResize(int w, int h) : ICmd
     {
         public readonly int W = w, H = h;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self) => self.ResizeInternal(W, H);
+        public void Exec(GL gl, CodeDrawLayer self) => self.ResizeInternal(W, H);
     }
 
     private sealed class CmdSetClearFirst : ICmd
     {
         public bool Enabled;
-        public void Exec(GL gl, DrawLayer.CodeDrawLayer self) => self._clearFirst = Enabled;
+        public void Exec(GL gl, CodeDrawLayer self) => self._clearFirst = Enabled;
     }
 }
--- tests/CodeDraw.Net.Tests.Manual/Prototypes/DrawLayer/CodeDrawLayer.Graphics.cs
+++ tests/CodeDraw.Net.Tests.Manual/Prototypes/DrawLayer/CodeDrawLayer.Graphics.cs
@@ -2,22 +2,22 @@
 
 public sealed unsafe partial class CodeDrawLayer
 {
-    public void DrawLayer(DrawLayer.CodeDrawLayer src) => Enqueue(new CmdLayer { Src = src });
+    public void DrawLayer(CodeDrawLayer src) => Enqueue(new CmdLayer { Src = src });
 
-    public void DrawLayer(DrawLayer.CodeDrawLayer src, RectF dstRect)
+    public void DrawLayer(CodeDrawLayer src, RectF dstRect)
         => Blit(src).Place(dstRect).Draw();
 
-    public void DrawLayer(DrawLayer.CodeDrawLayer src, RectF dstRect, BlendMode blend)
+    public void DrawLayer(CodeDrawLayer src, RectF dstRect, BlendMode blend)
         => Blit(src).Place(dstRect).Blend(blend).Draw();
 
-    public void DrawLayer(DrawLayer.CodeDrawLayer src, RectF srcRect, bool fitToTarget)
+    public void DrawLayer(CodeDrawLayer src, RectF srcRect, bool fitToTarget)
     {
         var dst = fitToTarget ? FullRect : new RectF(0, 0, srcRect.W, srcRect.H);
         Blit(src).Crop(srcRect).Place(dst).Draw();
     }
 
-    public void DrawLayer(DrawLayer.CodeDrawLayer src, RectF srcRect, RectF dstRect)
+    public void DrawLayer(CodeDrawLayer src, RectF srcRect, RectF dstRect)
         => Blit(src).Crop(srcRect).Place(dstRect).Draw();
 
-    public BlitSrcStage Blit(DrawLayer.CodeDrawLayer src) => new(this, src);
+    public BlitSrcStage Blit(CodeDrawLayer src) => new(this, src);
 }
